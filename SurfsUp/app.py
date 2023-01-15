892e6361d7f982e615d2724b561fca04a585cedf
ericlsimon <ericlsimon98@gmail.com>
2023-01-15 12:12:51 -0600

    final commit

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -23,6 +23,9 @@ Base.prepare(autoload_with=engine)
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
 #################################################
 # Flask Setup
 #################################################
@@ -46,8 +49,6 @@ def home():
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     # Create our session (link) from Python to the DB
-    session = Session(engine)
-    """Return converted Prcp DF to Dictionary"""
     # Query all passengers
     most_recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
     # Calculate the date one year from the last date in data set.
@@ -61,40 +62,59 @@ def precipitation():
     measurements_df.set_index('Date', inplace=True)
     # Sort the dataframe by date
     measurements_df.sort_values(by='Date')
-    #close
-    session.close()
     #convert to dicts
     prcp_dict = measurements_df.to_dict()
+    #close
+    session.close()
     #jsonify
     return jsonify(prcp_dict)
 
 @app.route("/api/v1.0/stations")
 def stations():
     # Create our session (link) from Python to the DB
-    session = Session(engine)
+    # session = Session(engine)
     # Return a JSON list of stations from the dataset.
-    station_list = session.query(Station.station,Station.name)
+    station_list = session.query(Station.station,Station.name).all()
     #close
     session.close()
     #jsonify
-    return jsonify(station_list)
+    results = np.ravel(station_list)
+    return jsonify(stations = results)
 
 @app.route("/api/v1.0/tobs")
 def tobs():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
     # Calculate the date one year from the last date in data set.
     year_ago_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
     # Query the dates and temperature observations of the most-active station for the previous year of data.
     temp_data = session.query(Measurement.tobs).filter(Measurement.station == 'USC00519281').filter(Measurement.date >= year_ago_date).all()
-    #close
     session.close()
     #jsonify
-    return jsonify(temp_data) 
+    tobs_info = []
+    for date,tobs in temp_data:
+        tobs_dic = {}
+        tobs_dic["date"] = date
+        tobs_dic["tobs"] = tobs
+        tobs_info.append(tobs_dic)
+    return jsonify(tobs_info)
+   
+@app.route("/api/v1.0/start")
+def start():
+    start_date = dt.date(2016, 12, 19)
+    # Return JSON list of the min temp, the avg temp, and max temp for a specified start or start-end range
+    start = session.query(func.min(Measurement.tobs),func.max(Measurement.tobs),func.avg(Measurement.tobs)).filter(Measurement.date >= start_date).all()
+    session.close()
+    jsonify_start = list(np.ravel(start))
+    return jsonify(jsonify_start)
+
+@app.route("/api/v1.0/<start>/<end>")
+def start_end():
+    start_date = dt.date(2016, 12, 19)
+    end_date = dt.date(2017, 1, 28)
+    start_end = session.query(func.min(Measurement.tobs),func.max(Measurement.tobs),func.avg(Measurement.tobs)).filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
+    session.close()
+    jsonify_start_end = list(np.ravel(start_end))
+    return jsonify(jsonify_start_end)
 
 if __name__ == '__main__':
-    app.run(debug=True)      
-
-# @app.route("/api/v1.0/start")
-# def start():
+    app.run(debug=True)  
 
